d9f894b2e22398d70b021a9b81a34d6c7062c773
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-10-29 17:18:13 -0400

    set up local environment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,10 +4,13 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const uri = import.meta.env.VITE_API_LINK;
 const client = new ApolloClient({
-  uri: "http://localhost:3000/graphql",
+  uri,
   cache: new InMemoryCache(),
 });
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ApolloProvider client={client}>
