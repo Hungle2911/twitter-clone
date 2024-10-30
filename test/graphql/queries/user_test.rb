fbc8ec16cb723a79a0138d6cb6b4c7fbe4705e74
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-10-29 23:48:01 -0400

    test: add test for retrieving user

--- test/graphql/queries/user_test.rb
+++ test/graphql/queries/user_test.rb
@@ -1,1 +1,26 @@
+class UserTest < ActiveSupport::TestCase
+  test 'retrieve user info' do
+    user = User.create!(username: "Testuser12", email: "testuser@example.com", password: "password")
 
+    query = <<~GQL
+      query GetUser($id: ID!) {
+        user(id: $id) {
+          id
+          username
+          email
+        }
+      }
+    GQL
+
+    # Execute the query, passing in the user ID as a variable
+    result = TwitterCloneSchema.execute(query, variables: { id: user.id })
+
+    # Extract data from the result
+    user_data = result["data"]["user"]
+
+    # Perform assertions to verify data is correct
+    assert_equal user.id.to_s, user_data["id"]
+    assert_equal user.username, user_data["username"]
+    assert_equal user.email, user_data["email"]
+  end
+end
